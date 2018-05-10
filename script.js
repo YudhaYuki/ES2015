888fc0466576d0bc3deb0e642ed0f0d323d56c54
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-10 15:29:46 +0200

    Variable Declaration with let and const - FUNCTION SCOPE - VERSION I - Works for both

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // LET and CONST
 
+/*
 // ES5
 var name5 = 'Yudha Yuki';
 var age = 23;
@@ -14,3 +15,32 @@ let age6 = 23;
 name6 = 'Yudha Miller';
 
 console.log(name6);
+*/
+
+
+// FUNCTION
+
+// ES5
+function driversLicence5(passedTest) {
+    if (passedTest) {
+        var firstName = 'John';
+        var yearOfBirth = 1990;
+
+        console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+    }
+}
+
+driversLicence5(true);
+
+
+// ES6
+function driversLicence6(passedTest) {
+    if (passedTest) {
+        let firstName = 'John';
+        const yearOfBirth = 1990;
+
+        console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+    }
+}
+
+driversLicence6(true);
