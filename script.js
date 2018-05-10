f2583a90a541a26a179aeb25182d25d7acf5d7d1
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-10 15:24:01 +0200

    Variable Declaration with let and const - Comparing vriables with VAR, CONST and LET

---
+++ script.js
@@ -0,0 +1,16 @@
+// LET and CONST
+
+// ES5
+var name5 = 'Yudha Yuki';
+var age = 23;
+name5 = 'Yudha Miller';
+
+console.log(name5);
+
+
+// ES6
+const name6 = 'Yudha Yuki';
+let age6 = 23;
+name6 = 'Yudha Miller';
+
+console.log(name6);
