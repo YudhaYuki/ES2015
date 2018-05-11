6017006020d0e6c00446f8a4fe4b7e2ca9065568
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-11 11:49:44 +0200

    ARRAYS - LOOP to change background color with CONTINUE method

--- script.js
+++ script.js
@@ -3,19 +3,65 @@
 const boxes = document.querySelectorAll('.box');
 
 // ES5
-/*
+
 var boxesArr5 = Array.prototype.slice.call(boxes);
 boxesArr5.forEach(function(cur) {
     cur.style.backgroundColor = 'dodgerblue';
 })
-*/
+
 
 
 // ES6
-/*
+
 var boxesArr6 = Array.from(boxes);
 boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
-*/
+
 
 // ES6 even shorter
-Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+// Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+
+
+
+
+
+// ES5 - ARRAY - LOOP
+/*
+for (var i = 0; i < boxesArr5.length; i++) {
+
+    if (boxesArr5[i].className === 'box blue') {
+        continue;
+        // break;
+    }
+
+    boxesArr5[i].textContent = 'I changed to blue !';
+}
+*/
+
+
+
+
+// ES6 - ARRAY - LOOP
+/*
+for (const cur of boxesArr6) {
+    
+    if (cur.className === 'box blue') {
+        continue;
+    }
+    
+    cur.textContent = 'I changed to blue !';
+}
+*/
+
+
+// ES6 - ARRAY - LOOP - SHORTEN
+
+for (const cur of boxesArr6) {
+    
+    if (cur.className.includes('blue')) {
+        continue;
+    }
+    
+    cur.textContent = 'I changed to blue !';
+
+}
