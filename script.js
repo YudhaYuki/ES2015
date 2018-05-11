48e8395a4a8a4b580699dba3c800ccf7df53c737
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-11 14:24:46 +0200

    Rest Parameter - Example I , Adding Parameter

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Rest Parameter
 
+/*
 // ES5
 function isFullAge5() {
     // console.log(arguments);
@@ -21,3 +22,30 @@ function isFullAge6(...years) {
 }
 
 isFullAge6(1990, 1999, 1965, 2016, 1987);
+*/
+
+
+
+
+
+// ES5
+function isFullAge5(limit) {
+    // console.log(arguments);
+    var argsArr = Array.prototype.slice.call(arguments, 1);
+
+    argsArr.forEach(function(cur) {
+        console.log((2016 - cur) >= limit);
+    })
+}
+
+isFullAge5(16, 1990, 1999, 1965);
+// isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+
+
+// ES6
+function isFullAge6(limit, ...years) {
+    years.forEach(cur => console.log((2016 - cur) >= limit));
+}
+
+isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
