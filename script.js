f6f6b2b7a1a027e50b515b3947bf0897e6ed042d
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-11 10:49:29 +0200

    Destructuring - OBJECT

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Desctructuring
 
-// ES5 
+// ES5 - ARRAY
 var john = ['John', 26];
 // var name = john[0];
 // var age = john[1];
@@ -9,8 +9,27 @@ var john = ['John', 26];
 // console.log(age);
 
 
-// ES6 
+// ES6 - ARRAY
 const [name, age] = ['John', 26];
-
 console.log(name);
 console.log(age);
+
+
+
+// ES6 - Object (Must be same KEY NAME)
+const obj = {
+    firstName: 'John',
+    lastName: 'Smith'
+};
+
+const {firstName, lastName} = obj;
+console.log(firstName);
+console.log(lastName);
+
+// Use different KEYNAME
+const {firstName: a, lastName: b} = obj;
+console.log(a);
+console.log(b);
+
+
+
